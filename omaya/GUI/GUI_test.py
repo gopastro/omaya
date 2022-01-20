6829b62cc567dad17e5a412484d1afa7a4c84f5a
Ananya Mac <ananyapriyad@umass.edu>
2022-01-20 13:04:11 -0500

    GUI test

---
+++ omaya/GUI/GUI_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+#general
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pl
+
+#dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output,State
+import plotly.express as px
+import plotly.graph_objects as go
